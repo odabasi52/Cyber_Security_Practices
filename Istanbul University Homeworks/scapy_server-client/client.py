8f90dbe837ec6c3ead000e482e1dac8cddebcb6f
root <root@talha.root>
2023-08-26 12:01:59 +0300

    send pcap scapy

--- Istanbul University Homeworks/scapy_server-client/client.py
+++ Istanbul University Homeworks/scapy_server-client/client.py
@@ -1,9 +1,16 @@
 import scapy.all as scp
 import random as r
 import argparse
+from pathlib import Path
+import netifaces as ni
+from getmac import get_mac_address
+from time import sleep
 
 class Client:
-    def __init__(self, src_ip, dst_ip, dst_port):
+    def __init__(self, src_ip, dst_ip, dst_port,iface, pcap=None, ):
+        self.dstmac = get_mac_address(ip=dst_ip)
+        self.iface=iface
+        self.pcap = pcap
         self.src_ip = src_ip
         self.dst_ip = dst_ip
         self.dst_port = dst_port
@@ -22,13 +29,58 @@ class Client:
             seq_num = syn_ack[scp.TCP].seq
             ack_num = syn_ack[scp.TCP].ack
             ack = scp.IP(src=self.src_ip, dst=self.dst_ip) / scp.TCP(sport=self.src_port, dport=self.dst_port, flags="A", seq=ack_num, ack=seq_num + 1)
-            scp.send(ack)
-            print("ACK sent to", self.dst_ip)
-            self.data_sender_loop(ack_num, seq_num)
+            if self.pcap == None:
+                scp.send(ack/scp.Raw(load="1"), iface=self.iface)
+                print("ACK (1) sent to", self.dst_ip)
+                self.send_user_input(ack_num, seq_num)
+            else:
+                scp.send(ack/scp.Raw(load="2"), iface=self.iface)
+                print("ACK (2) sent to", self.dst_ip)
+                self.send_pcap(ack[scp.TCP].ack, ack[scp.TCP].seq,)
         else:
             print("No SYN-ACK received.")
 
-    def data_sender_loop(self, ack_num, seq_num):
+    def send_pcap(self, ack_num, seq_num,):
+        print(f"sending pcap packets to {self.dst_ip}:{self.dst_port}")
+        packs = scp.rdpcap(self.pcap)
+
+        new_src_mac = get_mac_address(ip="0.0.0.0")
+        new_dst_mac = self.dstmac
+
+        for i, pkt in enumerate(packs):
+            if i > 0:
+                ack_num += 1
+            pkt[scp.Ether].src = new_src_mac
+            pkt[scp.Ether].dst = new_dst_mac
+
+            if scp.IP in pkt:
+                pkt[scp.IP].src= self.src_ip 
+                pkt[scp.IP].dst= self.dst_ip
+            if scp.TCP in pkt:
+                pkt[scp.TCP].sport = self.src_port
+                pkt[scp.TCP].dport = self.dst_port
+                pkt[scp.TCP].ack = ack_num
+                pkt[scp.TCP].seq = seq_num
+            while True:
+                scp.sendp(pkt, iface=self.iface)
+                ack_pkt = scp.sniff(timeout=10, filter=f"src {self.dst_ip} and dst {self.src_ip} and port {self.dst_port} and port {self.src_port}", count=1)
+                if ack_pkt is not None and len(ack_pkt) > 0:
+                    if scp.TCP in ack_pkt[0] and ack_pkt[0][scp.TCP].flags == "A":
+                        print("ACK received, server has pcap packet.")
+                        break
+                
+            seq = ack_pkt[0][scp.TCP].ack
+            ack = ack_pkt[0][scp.TCP].seq + 1
+
+        print("Sent FIN packet to server")
+        data_pkt = scp.IP(src=self.src_ip, dst=self.dst_ip)/scp.TCP(sport=self.src_port, dport=self.dst_port, flags="F", seq=seq, ack=ack)
+        scp.send(data_pkt, iface=self.iface)
+        fin_ack_pkt = scp.sniff(filter=f"tcp and src {self.dst_ip} and dst {self.src_ip} and port {self.dst_port} and port {self.src_port}", count=1)[0]
+        if scp.TCP in fin_ack_pkt and fin_ack_pkt[scp.TCP].flags == "FA":
+            print("FIN/ACK packet received from server, session terminated")
+            exit()
+
+    def send_user_input(self, ack_num, seq_num):
         print("exit or quit to terminate session\n--------------------\n")
         while True:
             try:
@@ -44,7 +96,7 @@ class Client:
             else:
                 data_pkt = scp.IP(src=self.src_ip, dst=self.dst_ip)/scp.TCP(sport=self.src_port, dport=self.dst_port, flags="PA", seq=ack_num, ack=seq_num) / scp.Raw(load=user_input)
                 ack_num += len(user_input)
-            scp.send(data_pkt)
+            scp.send(data_pkt, iface=self.iface)
 
             
 
@@ -58,16 +110,41 @@ class Client:
 def arguments():
     parser = argparse.ArgumentParser(description="TCP Connection")
     parser.add_argument('-s', '--src', help="source ip address (can be spoofed ip)") 
-    parser.add_argument('-d', '--dstip', help="destination ip address") 
+    parser.add_argument('-d', '--dstip', help="destination ip address")
+    parser.add_argument('-i', '--iface', help="interface to send data")
     parser.add_argument('-p', '--port', help="destination port address") 
+    parser.add_argument('-c', '--pcap', help="path of .pcap", default=None) 
     return parser.parse_args()
 
 if __name__ == "__main__":
     try:
         args = arguments()
-        client = Client(args.src,args.dstip, int(args.port))
+
+        if args.iface in ni.interfaces():
+            print(f"The interface '{args.iface}' is valid.")
+        else:
+            print("interface not found.")
+            exit()
+
+        pcap = None
+        if not args.pcap == None:
+            pcap = Path(args.pcap)
+            if pcap.is_file():
+                print("File path is OK, file exists.")
+                if pcap.suffix == ".pcap":
+                    print("file is pcap, program will be executed on pcap sending mode.")
+                else:
+                    print("file is not pcap, proram will be executed on user input mode.")
+            else:
+                print("Given path is wrong. No file found. Program will be executed on user input mode.")
+        else:
+            print("Pcap option is not used. Program will be executed on user input mode.")
+
+        
+        client = Client(args.src,args.dstip, int(args.port), args.iface, args.pcap, )
         client.establish_tcp_connection()
-    except:
+    except Exception as e:
+        print(e)
         print("--help/-h to get information about usage")
     
     
--- Istanbul University Homeworks/scapy_server-client/server.py
+++ Istanbul University Homeworks/scapy_server-client/server.py
@@ -7,9 +7,70 @@ class Server:
         self.lport = lport
 
     def establish_connection(self):
-        scp.sniff(filter=f"tcp and port {self.lport}", prn=self.handle_client)        
+        scp.sniff(filter=f"tcp and port {self.lport}", prn=self.handle_client)
+
+    def terminate_session(self,pkt,ack_num,seq_num):
+        ip = scp.IP(src=pkt[scp.IP].dst, dst=pkt[scp.IP].src)
+        tcp = scp.TCP(sport=pkt[scp.TCP].dport, dport=pkt[scp.TCP].sport, flags="FA", seq=ack_num , ack=seq_num)
+        fyn_ack = ip / tcp
+        scp.send(fyn_ack)
+        print("FYN/ACK packet is sent to client. Session terminated")
+        exit()
+
+    def user_input_mode(self, pkt, seq_num, ack_num):
+        global i
+        while True:
+            i += 1
+            data_pkt = scp.sniff(filter=f"tcp and src {pkt[scp.IP].src} and dst {pkt[scp.IP].dst} and port {pkt[scp.TCP].dport} and port {pkt[scp.TCP].sport}", count=1)[0]
+            if scp.TCP in data_pkt and data_pkt[scp.TCP].flags == "PA":
+                client_ip = data_pkt[scp.IP].src
+                client_data = data_pkt[scp.Raw].load.decode("utf-8")
+                print(f"{client_ip} has sent: {client_data}")
+                seq_num += (len(client_data))
+                if i > 1: 
+                    ack_num += 1
+                ip = scp.IP(src=pkt[scp.IP].dst, dst=pkt[scp.IP].src)
+                tcp = scp.TCP(sport=pkt[scp.TCP].dport, dport=pkt[scp.TCP].sport, flags="A", seq=ack_num , ack=seq_num)
+                ack = ip / tcp
+                scp.send(ack)
+            elif scp.TCP in data_pkt and data_pkt[scp.TCP].flags == "F":
+                client_ip = data_pkt[scp.IP].src
+                print(f"{client_ip} has sent FIN packet to terminate session")
+                seq_num += 1
+                if i > 1: 
+                    ack_num += 1
+                self.terminate_session(data_pkt, ack_num, seq_num)    
+
+    def pcap_write_mode(self, pkt, seq_num, ack_num):
+        global i
+        packets = []
+        while True:
+            i += 1
+            data_pkt = scp.sniff(filter=f"ether src {pkt[scp.Ether].src} and ether dst {pkt[scp.Ether].dst}", count=1)[0]
+            if scp.TCP in data_pkt and data_pkt[scp.TCP].flags == "PA":
+                packets.append(data_pkt)
+                client_ip = data_pkt[scp.IP].src
+                client_data = data_pkt[scp.Raw].load
+                print(f"{client_ip} has sent a pcap packet")
+                seq_num += (len(client_data))
+                if i > 1: 
+                    ack_num += 1
+                ip = scp.IP(src=pkt[scp.IP].dst, dst=pkt[scp.IP].src)
+                tcp = scp.TCP(sport=pkt[scp.TCP].dport, dport=pkt[scp.TCP].sport, flags="A", seq=ack_num , ack=seq_num)
+                ack = ip / tcp
+                scp.send(ack)
+            elif scp.TCP in data_pkt and data_pkt[scp.TCP].flags == "F":
+                client_ip = data_pkt[scp.IP].src
+                print(f"{client_ip} has sent FIN packet to terminate session")
+                scp.wrpcap("received.pcap", packets)
+                print("All received packets are saved as received.pcap")
+                seq_num += 1
+                if i > 1: 
+                    ack_num += 1
+                self.terminate_session(data_pkt, ack_num, seq_num)
 
     def handle_client(self, pkt):
+        mode = "0"
         if scp.TCP in pkt and pkt[scp.TCP].flags == "S":
             print("SYN received from", pkt[scp.IP].src)
             seq_num = pkt[scp.TCP].seq
@@ -19,39 +80,19 @@ class Server:
             scp.send(syn_ack)
             print("SYN-ACK sent to", pkt[scp.IP].src)
         elif scp.TCP in pkt and pkt[scp.TCP].flags == "A":
+            global i
+            i = 0
+
             print("ACK received from", pkt[scp.IP].src)
             print("TCP connection established")
+            mode = pkt[scp.Raw].load.decode("utf-8")
             seq_num = pkt[scp.TCP].seq
             ack_num = pkt[scp.TCP].ack
-            i = 0
-            while True:
-                i += 1
-                data_pkt = scp.sniff(filter=f"tcp and src {pkt[scp.IP].src} and dst {pkt[scp.IP].dst} and port {pkt[scp.TCP].dport} and port {pkt[scp.TCP].sport}", count=1)[0]
-                if scp.TCP in data_pkt and data_pkt[scp.TCP].flags == "PA":
-                    client_ip = data_pkt[scp.IP].src
-                    client_data = data_pkt[scp.Raw].load.decode("utf-8")
-                    
-                    print(f"{client_ip} has sent: {client_data}")
-
-                    seq_num += (len(client_data))
-                    if i > 1: 
-                        ack_num += 1
-                    ip = scp.IP(src=pkt[scp.IP].dst, dst=pkt[scp.IP].src)
-                    tcp = scp.TCP(sport=pkt[scp.TCP].dport, dport=pkt[scp.TCP].sport, flags="A", seq=ack_num , ack=seq_num)
-                    ack = ip / tcp
-                    scp.send(ack)
-                elif scp.TCP in data_pkt and data_pkt[scp.TCP].flags == "F":
-                    client_ip = data_pkt[scp.IP].src
-                    print(f"{client_ip} has sent FIN packet to terminate session")
-                    seq_num += 1
-                    if i > 1: 
-                        ack_num += 1
-                    ip = scp.IP(src=pkt[scp.IP].dst, dst=pkt[scp.IP].src)
-                    tcp = scp.TCP(sport=pkt[scp.TCP].dport, dport=pkt[scp.TCP].sport, flags="FA", seq=ack_num , ack=seq_num)
-                    fyn_ack = ip / tcp
-                    scp.send(fyn_ack)
-                    print("FYN/ACK packet is sent to client. Session terminated")
-                    exit()
+            
+            if mode == "1":
+                self.user_input_mode(pkt, seq_num, ack_num)     
+            elif mode == "2":
+                self.pcap_write_mode(pkt, seq_num, ack_num)
             
     
 if __name__ == "__main__":
