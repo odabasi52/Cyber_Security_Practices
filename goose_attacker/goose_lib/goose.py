61f8e0abc29bdbbc354d906573cd0fbfce7a9d57
odabasi52 <ordulubaba0034@gmail.com>
2023-11-20 15:02:49 +0300

    byteorder=big

--- goose_attacker/goose_lib/goose.py
+++ goose_attacker/goose_lib/goose.py
@@ -1,18 +1,18 @@
 class GOOSE:
     def __init__(self, appId, gocbRef, timeAllowedToLive, datSet, goId, t, 
                  stNum, sqNum, simulation, confRev, ndsCom, numDatSetEntries, allData):
-        self.appId = b"\x00" + appId.to_bytes(length=1)
+        self.appId = b"\x00" + appId.to_bytes(length=1, byteorder="big")
         self.gocbRef = b"\x00\x91\x00\x00\x00\x00a\x81\x86\x80\x1a" + gocbRef
-        self.timeAllowedToLive = b"\x81\x03\x00" + timeAllowedToLive.to_bytes(length=2)
+        self.timeAllowedToLive = b"\x81\x03\x00" + timeAllowedToLive.to_bytes(length=2, byteorder="big")
         self.datSet = b"\x82\x18" + datSet
         self.goId = b"\x83\x0b" + goId
-        self.t =  b"\x84\x08" +  int(t).to_bytes(length=4) + b"\x00\x00\x00\x00"
-        self.stNum = b"\x85\x01" + stNum.to_bytes(length=1)
-        self.sqNum = b"\x86\x01" + sqNum.to_bytes(length=1)
-        self.simulation = b"\x87\x01" + simulation.to_bytes(length=1)
-        self.confRev = b"\x88\x01" + confRev.to_bytes(length=1)
-        self.ndsCom = b"\x89\x01" + ndsCom.to_bytes(length=1)
-        self.numDatSetEntries = b"\x8a\x01" + numDatSetEntries.to_bytes(length=1)
+        self.t =  b"\x84\x08" +  int(t).to_bytes(length=4, byteorder="big") + b"\x00\x00\x00\x00"
+        self.stNum = b"\x85\x01" + stNum.to_bytes(length=1, byteorder="big")
+        self.sqNum = b"\x86\x01" + sqNum.to_bytes(length=1, byteorder="big")
+        self.simulation = b"\x87\x01" + simulation.to_bytes(length=1, byteorder="big")
+        self.confRev = b"\x88\x01" + confRev.to_bytes(length=1, byteorder="big")
+        self.ndsCom = b"\x89\x01" + ndsCom.to_bytes(length=1, byteorder="big")
+        self.numDatSetEntries = b"\x8a\x01" + numDatSetEntries.to_bytes(length=1, byteorder="big")
         self.allData = allData
         if self.allData != None:
             self.allData = b"\xab " + allData
