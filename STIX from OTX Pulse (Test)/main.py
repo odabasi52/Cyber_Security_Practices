1212f08c861a50ff2c0c74fb2396e342a15d3320
odabasi52 <ordulubaba0034@gmail.com>
2024-02-11 16:25:43 +0300

    TESTs

--- STIX from OTX Pulse (Test)/main.py
+++ STIX from OTX Pulse (Test)/main.py
@@ -7,12 +7,15 @@ class OTX_Test:
         self.otx = OTXv2(otx_api_key)
         
     def search_pulses(self, search, result_count=5):
-        self.pulses = self.otx.search_pulses(search, max_results=result_count)["results"]
+        SEARCH_PULSES = "{}/search/pulses".format("/api/v1")   
+        search_pulses_url = self.otx.create_url(SEARCH_PULSES, q=search, page=1, limit=25, sort="-modified")
+        self.pulses = self.otx._get_paginated_resource(search_pulses_url, max_results=result_count)["results"]
     
     def save_pulse_details_as_json(self):
         for i, json_data in enumerate(self.pulses):
-            pulse_details = self.otx.get_pulse_details(json_data["id"])
-            save_file = open(os.path.join(self.parent_dir_to_save_stix, f"{i}_stix.json"), "w")  
+            pulse_id = json_data["id"]
+            pulse_details = self.otx.get_pulse_details(pulse_id)
+            save_file = open(os.path.join(self.parent_dir_to_save_stix, f"{i}_{pulse_id}.json"), "w")  
             json.dump(pulse_details, save_file, indent = 6)  
             save_file.close()
 
